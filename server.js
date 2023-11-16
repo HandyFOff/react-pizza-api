091ae51e413d2f5d6ae8ebecd96740d1e12ccf0b
HandyF <86957456+HandyFOff@users.noreply.github.com>
2023-11-16 11:37:02 +0300

    changed test route 0.0.3

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const db = jsonServer.router('./db/index.json');
 const middlewares = jsonServer.defaults();
 
 app.use(middlewares);
-app.use(db);
+app.use("/api/", db);
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is live');
 })
